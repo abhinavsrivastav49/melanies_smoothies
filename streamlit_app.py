a44f2744d22bf57850196df2490a974fec3dd2f9
abhinavsrivastav49 <abhinav.srivastav49@gmail.com>
2025-04-15 16:52:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,15 +15,14 @@ st.write("The name on your smoothie will be:", name_on_order)
 
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name')).to_pandas()
-fruit_options = my_dataframe['Fruit_Name'].tolist()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
 
 
 
 ingredients_list = st.multiselect(
-    "Choose up to 5 ingredients:",
-    fruit_options,
-    max_selections=5
+"Choose up to 5 ingredients:"
+, my_dataframe
+, max_selections = 5
 )
 
 if ingredients_list:
@@ -35,8 +34,8 @@ if ingredients_list:
         #st.write(ingredients_string)
 
 
-       my_insert_stmt = "INSERT INTO smoothies.public.orders (ingredients, name_on_order) VALUES (%s, %s)"
-       session.sql(my_insert_stmt, [ingredients_string, name_on_order]).collect()
+        my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 
         #st.write(my_insert_stmt)
     
@@ -47,6 +46,6 @@ if ingredients_list:
     
             st.success(f"Your smoothie has been ordered, {name_on_order}!", icon="✅")
 
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
